81c27976be2456192b2786240ea8b949994a49a9
Shivanee <Shivaneej02@gmail.com>
2019-11-09 15:18:58 +0530

    Spawning all assets

---
+++ Assets/Scripts/AugmentedImageExampleController.cs
@@ -0,0 +1,59 @@
+﻿namespace EducatAR
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using GoogleARCore;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class AugmentedImageExampleController : MonoBehaviour
+    {
+        public List<AugmentedImageVisualizer> AugmentedImageVisualizerPrefab = new List<AugmentedImageVisualizer>();
+        public GameObject FitToScanOverlay;
+        private Dictionary<int, AugmentedImageVisualizer> m_Visualizers
+        = new Dictionary<int, AugmentedImageVisualizer>();
+        private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
+
+        // Update is called once per frame
+        void Update()
+        {   
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+            if (Session.Status != SessionStatus.Tracking)
+            {
+                return;
+            }
+            Session.GetTrackables<AugmentedImage>(m_TempAugmentedImages, TrackableQueryFilter.Updated);
+            foreach (var image in m_TempAugmentedImages)
+            {
+                AugmentedImageVisualizer visualizer = null;
+                m_Visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+                if (image.TrackingState == TrackingState.Tracking && visualizer == null)
+                {
+                    Anchor anchor = image.CreateAnchor(image.CenterPose);
+                    visualizer = (AugmentedImageVisualizer)Instantiate(AugmentedImageVisualizerPrefab[image.DatabaseIndex], anchor.transform);
+                    visualizer.Image = image;
+                    m_Visualizers.Add(image.DatabaseIndex, visualizer);
+                }
+                else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+                {
+                    m_Visualizers.Remove(image.DatabaseIndex);
+                    GameObject.Destroy(visualizer.gameObject);
+                }
+            }
+            foreach (var visualizer in m_Visualizers.Values)
+            {
+                if (visualizer.Image.TrackingState == TrackingState.Tracking)
+                {
+                    FitToScanOverlay.SetActive(false);
+                    return;
+                }
+            }
+            FitToScanOverlay.SetActive(true);
+        }
+    }
+}
+
---
+++ Assets/Scripts/AugmentedImageVisualizer.cs
@@ -0,0 +1,55 @@
+﻿namespace EducatAR
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using GoogleARCore;
+    using GoogleARCoreInternal;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class AugmentedImageVisualizer : MonoBehaviour
+    {
+
+        public AugmentedImage Image;
+        public GameObject[] Models;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Image == null || Image.TrackingState != TrackingState.Tracking)
+            {
+                Models[Image.DatabaseIndex].SetActive(false);
+                //Models[0].SetActive(false);
+                //Models[1].SetActive(false);
+                return;
+            }
+            if (Image == null || Image.TrackingState == TrackingState.Stopped)
+            {
+                Models[Image.DatabaseIndex].SetActive(false);
+                //Models[0].SetActive(false);
+                //Models[1].SetActive(false);
+                return;
+            }
+            if (Image == null || Image.TrackingState == TrackingState.Paused)
+            {
+                Models[Image.DatabaseIndex].SetActive(false);
+                //Models[0].SetActive(false);
+                //Models[1].SetActive(false);
+                return;
+            }
+            Pose _centerPose = Image.CenterPose;
+            Models[Image.DatabaseIndex].transform.localPosition = _centerPose.position;
+            Models[Image.DatabaseIndex].transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+            Models[Image.DatabaseIndex].SetActive(true);
+        }
+    }
+}
+
